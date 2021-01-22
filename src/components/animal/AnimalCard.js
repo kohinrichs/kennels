253386b9b09c4f580baaeaa2e4f3b73f4cf643eb
kohinrichs <ko.hinrichs@gmail.comcdgit config --global user.email ko.hinrichs@gmail.com>
2021-01-22 13:51:09 -0600

    Added most of chapeter 8

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -1,9 +1,11 @@
 import React from "react"
 import "./Animal.css"
 
-export const AnimalCard = ({ animal }) => (
+export const AnimalCard = ({ animal, customer, location }) => (
     <section className="animal">
         <h3 className="animal__name">{animal.name}</h3>
-        <address className="location__address">{animal.location.name}</address>
+        <h4 className="animal__breed">{animal.breed}</h4>
+        <address className="location__address">{location.name}</address>
+        <h4 className="customer__name">{customer.name}</h4>
     </section>
 )
