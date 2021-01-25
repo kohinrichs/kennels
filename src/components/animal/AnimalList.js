6e7ac9823fe13d1c63c792503b150097261388c8
kohinrichs <ko.hinrichs@gmail.comcdgit config --global user.email ko.hinrichs@gmail.com>
2021-01-25 10:28:31 -0600

    Added owner and location to animal list

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -5,6 +5,8 @@ import { CustomerContext } from "../customer/CustomerProvider"
 import { AnimalCard } from "./AnimalCard"
 import "./Animal.css"
 
+// Location is currently tied to Animals because of the expand in the Animal Provider. 
+// May need to refactor later on depending on what we need locations for/to do.
 export const AnimalList = () => {
   // This state changes when `getAnimals()` is invoked below
   const { animals, getAnimals } = useContext(AnimalContext)
@@ -16,17 +18,31 @@ export const AnimalList = () => {
     console.log("AnimalList: Initial render before data")
     getLocations()
     .then(getCustomers)
+    getCustomers()
     .then(getAnimals)
+    getAnimals()
   }, [])
 
-      animals.map(animal => {
-        const owner = customers.find(c => c.id === animal.customerId)
-        const clinic = locations.find(l => l.id === animal.locationId)
+  return (
+    <div className="animals">
+      {animals.map(animal => {
+          const owner = customers.find(c => c.id === animal.customerId)
+          const location = locations.find(l => l.id === animal.locationId)
 
-    return <AnimalCard key={animal.id}
-        location={clinic}
-        customer={owner}
-        animal={animal} />
-    }
+          return <AnimalCard key={animal.id} animal={animal} owner={owner} location ={location} />
+        })
+      }
+    </div>
   )
 }
+// animals.map(animal => {
+//   const owner = customers.find(c => c.id === animal.customerId)
+//   const clinic = locations.find(l => l.id === animal.locationId)
+
+// return <AnimalCard key={animal.id}
+//   location={clinic}
+//   customer={owner}
+//   animal={animal} />
+// }
+// )
+// }
